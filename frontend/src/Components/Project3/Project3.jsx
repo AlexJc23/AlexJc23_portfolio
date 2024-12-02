5a31286e53494d139922f295b86547e814a9ed09
Alex Carl <alexjcarl97@gmail.com>
2024-12-02 15:31:49 -0500

    ul bug

--- frontend/src/Components/Project3/Project3.jsx
+++ frontend/src/Components/Project3/Project3.jsx
@@ -12,7 +12,7 @@ const Project3 = () => {
                     <p>E-commerce platform built with JavaScript, Express, and React, allowing users to buy and sell custom-made clothing, accessories, and cosplay props. Users can list items for sale, interact with products through a shopping cart, reviews, and favorites, while instantly making their items available for others to explore..</p>
                 </div>
                 <div className="df-topright">
-                    <ul>
+                    <ul className="project-list">
                         <li><span className="span-text-main">Role: </span><p>Full Stack Developer</p></li>
                         <li><span className="span-text-main">Duration: </span><p>June 2024 - August 2024</p></li>
                         <li><span className="span-text-main">Responsibilities: </span><p>UX/Ui Design, Wireframing, Frontend/Backend Development </p></li>
