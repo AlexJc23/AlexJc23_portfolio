3c2c6d27d6e582b3d6bdc28a11136bbe338d14fe
Alex Carl <alexjcarl97@gmail.com>
2024-12-02 00:20:05 -0500

    new images

--- frontend/src/Components/Project1/Project1.jsx
+++ frontend/src/Components/Project1/Project1.jsx
@@ -16,12 +16,12 @@ const Project1 = () => {
                         <li><span className="span-text-main">Role: </span><p>Full Stack Developer</p></li>
                         <li><span className="span-text-main">Duration: </span><p>August 2024 - Now</p></li>
                         <li><span className="span-text-main">Responsibilities: </span><p>UX/Ui Design, Wireframing, Frontend/Backend Development </p></li>
- 
+
                     </ul>
                 </div>
                 </div>
                 <div >
-                    <img className="project-img" src="../src/assets/dfmockup.png" alt="DuelForge mockup image" />
+                    <img className="project-img" src="../src/assets/gamingdf.jpeg" alt="DuelForge mockup image" />
                 </div>
             </div>
 
