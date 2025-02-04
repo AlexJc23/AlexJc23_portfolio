257254236cb225b0decc51cf22538dfd2284a3f6
Alex Carl <alexjcarl97@gmail.com>
2025-02-03 19:17:46 -0500

    updated about in portfolio

--- frontend/src/Components/About/About.jsx
+++ frontend/src/Components/About/About.jsx
@@ -6,13 +6,13 @@ const About = () => {
         <div className="about-cont">
             <h3>Who Am I?</h3>
             <p>
-                Your friendly full-stack developer with a passion for React, Python, and all things tech! I bring ideas to life with a creative touch, blending design and development to build impactful, user-centric solutions that deliver results.
-                <br />
-                <br />
-                When I’m not coding something cool, you’ll find me geeking out over Marvel, leveling up in video games, hunting for rare Yu-Gi-Oh! cards, or creating mind-bending manipulation art for Instagram. Above all, I cherish spending quality time with my son and wife, making memories that last a lifetime.
-                <br />
-                <br />
-                I graduated high school at 16 and jumped straight into the real world, where I discovered my passion for tech. Now, I’m focused on building innovative solutions, with the ultimate dream of running my own software empire someday. Let’s team up and create something extraordinary together!
+            Your friendly full-stack developer with a passion for React, Python, and all things tech! I bring ideas to life with a creative touch, blending design and development to build impactful, user-centric solutions that deliver results. I’ve explored many fields—from architecture to graphic design—constantly seeking challenges that push me to grow. But nothing ever quite matched the excitement of coding. After I discovered web development and learned how to manipulate the DOM, it was like unlocking a new world. The logic, problem-solving, and endless possibilities sparked something in me that no other field had.
+            <br />
+            <br />
+            As I applied to different bootcamps, I encountered coding challenges that pushed me to my limits. But instead of backing down, I kept learning, determined to break through each problem. That drive has only grown stronger, fueling my transition into tech and shaping my journey as a developer. Now, I’m focused on building innovative solutions with the ultimate dream of running my own software empire someday.
+            <br />
+            <br />
+            When I’m not coding something cool, you’ll find me geeking out over Marvel, leveling up in video games, hunting for rare Yu-Gi-Oh! cards, or creating mind-bending manipulation art for Instagram. Above all, I cherish spending quality time with my son and wife, making memories that last a lifetime. Let’s team up and create something extraordinary together!
             </p>
         </div>
     );
